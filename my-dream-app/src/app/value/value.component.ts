6f5674f9bb4dcc33f1955f91ca907ef0e9680ffa
Sina Layazali <sina_unforgiven3@yahoo.com>
2019-12-29 06:13:27 +0330

    init project

---
+++ my-dream-app/src/app/value/value.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-value',
+  templateUrl: './value.component.html',
+  styleUrls: ['./value.component.css']
+})
+export class ValueComponent implements OnInit {
+values: any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getValues();
+  }
+
+  getValues() {
+    this.http.get('http://localhost:52986/weatherforecast').subscribe(resp => {
+      this.values = resp;
+    }, error => {
+      console.log('error');
+    });
+  }
+}
